ed8b32410b95dbc2a8ceaf877a1837db48f8e9ca
Asilver-jpg <asilver7493@gmail.com>
2020-05-12 13:57:02 -0400

    ?

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
 class Login extends React.Component {
+   
     state = {
-        // TODO: What needs to be represented in state for a fully controlled form?
+        name : "",
+        userName: "",
+        password: "",
+        confirmPassword:""
     }
+
+handleName = event =>{
+    this.setState({
+        name: event.target.value
+    })
+}
+
+handleUserName = event =>{
+    this.setState({
+        username: event.target.value
+    })
+}
+
+handlePassword = event =>{
+    this.setState({
+        password: event.target.value
+    })
+}
+handleConfirm = event =>{
+    this.setState({
+        confirmPassword: event.target.value
+    })
+}
 
     // TODO: What methods need to be created for a fully controlled form?
     // HINT: Use the line below to change the view when the form is submitted
@@ -14,11 +41,12 @@ class Login extends React.Component {
         return (
             <form className="vertical-flex">
                 <h2>Create an Account</h2>
-                <input placeholder="Name"/>
-                <input placeholder="Username"/>
-                <input placeholder="Password"/>
-                <input placeholder="Confirm Password"/>
+                <input type="text" onChange={event => this.handleName(event)} value={this.state.name} placeholder="Name"/>
+                <input type="text" onChange={event => this.handleUserName(event)} value={this.state.username} placeholder="Username"/>
+                <input type="text" onChange={event => this.handlePassword(event)} value={this.state.password} placeholder="Password"/>
+                <input type="text" onChange={event => this.handleConfirm(event)} value={this.state.confirmpassword} placeholder="Confirm Password"/>
                 <button type="submit">Submit</button>
+
             </form>
         )
     }
