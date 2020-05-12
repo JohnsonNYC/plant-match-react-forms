6d0d305b5da4a4ba6dbb6309a31cacf00e57ff89
diana2341 <dianaponce2341@gmail.com>
2020-05-12 13:59:02 -0400

    form

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 class Login extends React.Component {
     state = {
+        name:" ",
+        Username:" ",
+        password:" "
+
         // TODO: What needs to be represented in state for a fully controlled form?
     }
+    handleChange = event => {
+        this.setState({
+          [event.target.name]: event.target.value
+        })
+      }
+      handleSubmit = (event) => {
+          event.preventDefault()
+        this.props.changeView("home");
+    }
+
 
     // TODO: What methods need to be created for a fully controlled form?
     // HINT: Use the line below to change the view when the form is submitted
@@ -12,12 +26,12 @@ class Login extends React.Component {
     render(){
         // TODO: What additional attributes and event handlers are needed on each of the elements below?
         return (
-            <form className="vertical-flex">
+            <form className="vertical-flex" onSubmit={this.handleSubmit}>
                 <h2>Create an Account</h2>
-                <input placeholder="Name"/>
-                <input placeholder="Username"/>
-                <input placeholder="Password"/>
-                <input placeholder="Confirm Password"/>
+                <input placeholder="Name" id="name" name="name" type="text" onChange={this.handleChange} value={this.state.name}/>
+                <input placeholder="Username" id="username" name="username" type="text" onChange={this.handleChange} value={this.state.username}/>
+                <input placeholder="Password" id="password" name="password" type="text" onChange={this.handleChange} value={this.state.password}/>
+                <input placeholder="Confirm Password" id="password" name="password" type="text" onChange={this.handleChange} value={this.state.password}/>
                 <button type="submit">Submit</button>
             </form>
         )
