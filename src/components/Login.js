92ba7e9c123c149c145a53d02a47a56826d8f3cf
Lix311 <alex.sterrantino@gmail.com>
2020-05-12 13:46:58 -0400

    firstdel

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,23 +2,56 @@ import React from 'react';
 
 class Login extends React.Component {
     state = {
+        view:'home',
+        name:'Alex',
+        username: 'Alex',
+        password: 'password',
+        confirmpassword: 'confirm password'
         // TODO: What needs to be represented in state for a fully controlled form?
     }
 
     // TODO: What methods need to be created for a fully controlled form?
     // HINT: Use the line below to change the view when the form is submitted
     // this.props.changeView('home')
+    nameChange=(event)=>{
+        this.setState({name: event.target.value})
+        console.log(this.state)
+    }
+
+    usernameChange=(event)=>{
+        this.setState({username: event.target.value})
+    }
+
+    passwordChange=(event)=>{
+        this.setState({password: event.target.value})
+    }
+
+    confirmChange=(event)=>{
+        this.setState({confirmpassword: event.target.value})
+    }
+
+    handleSubmit=(event)=>{
+        event.preventDefault();
+        this.props.changeView('home')
+    }
+
+
+
+
+
+
+
 
     render(){
         // TODO: What additional attributes and event handlers are needed on each of the elements below?
         return (
             <form className="vertical-flex">
                 <h2>Create an Account</h2>
-                <input placeholder="Name"/>
-                <input placeholder="Username"/>
-                <input placeholder="Password"/>
-                <input placeholder="Confirm Password"/>
-                <button type="submit">Submit</button>
+                <input placeholder="Name" name='name' onChange={event => this.nameChange(event)} value={this.state.name}/>
+                <input placeholder="Username" name='username' onChange={event => this.usernameChange(event)} value={this.state.username}/>
+                <input placeholder="Password" name='password' onChange={event => this.passwordChange(event)} value={this.state.password}/>
+                <input placeholder="Confirm Password" name='confirm password' onChange={event => this.confirmChange(event)} value={this.state.confirmpassword}/>
+                <button onClick={this.handleSubmit} name='submit' type="submit">Submit</button>
             </form>
         )
     }
