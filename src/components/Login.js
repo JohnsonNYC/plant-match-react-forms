8e6cda301f0a7870c9026b7148f28357265c0c65
Samanthaponce5 <samanthaponce5@gmail.com>
2020-05-12 13:58:39 -0400

    almost

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,39 @@
 import React from 'react';
 
 class Login extends React.Component {
+
     state = {
+        name:'',
+        username:'',
+        password:'',
+        confirmPassword:''
         // TODO: What needs to be represented in state for a fully controlled form?
     }
 
+    handleChange = event => {
+        this.setState({
+          [event.target.name]: event.target.value
+        })
+      }
+    
     // TODO: What methods need to be created for a fully controlled form?
     // HINT: Use the line below to change the view when the form is submitted
     // this.props.changeView('home')
+    handleSubmit=(event)=>{
+        event.preventDefault()
+        this.props.changeView('home')
+    }
 
     render(){
         // TODO: What additional attributes and event handlers are needed on each of the elements below?
         return (
-            <form className="vertical-flex">
+            <form className="vertical-flex" onSubmit={this.handleSubmit}>
                 <h2>Create an Account</h2>
-                <input placeholder="Name"/>
-                <input placeholder="Username"/>
-                <input placeholder="Password"/>
-                <input placeholder="Confirm Password"/>
-                <button type="submit">Submit</button>
+                <input placeholder="Name" id="name" name="name" type="text" onChange={this.handleChange} value={this.state.name}/>
+                <input placeholder="Username" id="username" name="username" type="text" onChange={this.handleChange} value={this.state.username}/>
+                <input placeholder="Password" id="password" name="password" type="text" onChange={this.handleChange} value={this.state.password}/>
+                <input placeholder="Confirm Password" id="confirm password" name="comfirm password" type="text" onChange={this.handleChange} value={this.state.password}/>
+                <button  type="submit">Submit</button>
             </form>
         )
     }
