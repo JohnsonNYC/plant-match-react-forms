1cd17ac38e6f256b2c257c63eb7e20485755b1d5
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-05-17 19:14:37 -0400

    search working

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -5,18 +5,27 @@ import SearchForm from './SearchForm';
 
 class HomeView extends React.Component {
     state = {
-        plants: plants
+        plants: plants,
+        search: ''
     }
 
+    searchPlants = (event) => {
+        this.setState({search: event.target.value})
+    }
+
+
+
     render(){
+        console.log(this.state)
+        const searchedPlants = this.state.plants.filter(plant => plant.Common_Name.toLowerCase().includes(this.state.search.toLowerCase()))
         // TODO: In order to search, what state, methods and element attributes are needed? 
         // In order to render the correct plants, what calculations do you need to do and what props do you need to change below?
 
         return (
             <div>
-                <input placeholder="Search for Plants"/>
+                <input onChange={this.searchPlants} placeholder="Search for Plants"/>
                 {/* <SearchForm /> */} {/** DO NOT UNCOMMENT THIS LINE UNTIL WORKING ON ADVANCED DELIVERABLES */}
-                <MatchContainer plants={this.state.plants}/>
+                <MatchContainer plants={searchedPlants}/>
             </div>
         )
     }
