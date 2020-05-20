92d21f35d0ad81170b9ec2398447a42eba7f1642
JohnsonNYC <jkow95@gmail.com>
2020-05-19 21:24:14 -0400

    Core Deli Done

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -5,18 +5,27 @@ import SearchForm from './SearchForm';
 
 class HomeView extends React.Component {
     state = {
-        plants: plants
+        plants: plants,
+        search: ""
+    }
+    toggleSearch=(event)=>{  
+        this.setState({ 
+            search: event.target.value  
+        })
+        
     }
 
     render(){
         // TODO: In order to search, what state, methods and element attributes are needed? 
         // In order to render the correct plants, what calculations do you need to do and what props do you need to change below?
-
+        let filtered = this.state.plants.filter(plant =>{
+            return plant["Common_Name"].includes(this.state.search)
+        })
         return (
             <div>
-                <input placeholder="Search for Plants"/>
-                {/* <SearchForm /> */} {/** DO NOT UNCOMMENT THIS LINE UNTIL WORKING ON ADVANCED DELIVERABLES */}
-                <MatchContainer plants={this.state.plants}/>
+                <input placeholder="Search for Plants" value={this.state.search} onChange={this.toggleSearch}/>
+                {/* <SearchForm plants={this.state.plants} search={this.state.search}/> */}
+                <MatchContainer plants={this.state.search===""?this.state.plants: filtered} search={this.state.search}/>
             </div>
         )
     }
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,23 +2,39 @@ import React from 'react';
 
 class Login extends React.Component {
     state = {
-        // TODO: What needs to be represented in state for a fully controlled form?
+        name:"",
+        username:"",
+        password: "",
+        confirmPassword:""
     }
 
     // TODO: What methods need to be created for a fully controlled form?
     // HINT: Use the line below to change the view when the form is submitted
     // this.props.changeView('home')
+    toggleChange=(event)=>{
+        if(event.target.placeholder==='Name'){
+            this.setState({ name: event.target.value  });
+        }else if(event.target.placeholder==='Username'){
+            this.setState({ username: event.target.value  });
+        }else if(event.target.placeholder==='Password'){
+            this.setState({ password: event.target.value  });
+        }else if(event.target.placeholder==='Confirm Password'){
+            this.setState({ confirmPassword: event.target.value  });
+        }
+    }
 
     render(){
+        const {name,username,password,confirmPassword} = this.state
+        console.log(this.state)
         // TODO: What additional attributes and event handlers are needed on each of the elements below?
         return (
             <form className="vertical-flex">
                 <h2>Create an Account</h2>
-                <input placeholder="Name"/>
-                <input placeholder="Username"/>
-                <input placeholder="Password"/>
-                <input placeholder="Confirm Password"/>
-                <button type="submit">Submit</button>
+                <input placeholder="Name" value={name} onChange={this.toggleChange}/>
+                <input placeholder="Username" value={username} onChange={this.toggleChange}/>
+                <input placeholder="Password" value={password} onChange={this.toggleChange}/>
+                <input placeholder="Confirm Password" value={confirmPassword} onChange={this.toggleChange}/>
+                <button type="submit" onSubmit={() => this.props.changeView('home')}>Submit</button>
             </form>
         )
     }
